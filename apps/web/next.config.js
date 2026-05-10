a0057b04ba16a9164688ddd02673291bce2a46a6
Eline De Buyser <elinedebuyser@Smexmachine-2.local>
2026-05-10 18:08:42 +0800

    web: create redirects to canva for our food menus

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -20,6 +20,14 @@ const nextConfig = {
             destination: '/',
             permanent: true, // send 301 redirect
         }, {
+            source: '/menu',
+            destination: 'https://www.canva.com/design/DAGhYRNTFgc/BkFZ5gh3rJnTuon_lI1OCg/view?utm_content=DAGhYRNTFgc&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hffa6aee53f',
+            permanent: true,
+        }, {
+            source: '/junglenookmenu',
+            destination: 'https://www.canva.com/design/DAGZCJNzgeA/L9Tz7Y8pdyin1PZQ9KeN7Q/view?utm_content=DAGZCJNzgeA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h99deea3dde',
+            permanent: true,
+        }, {
             // looks for any path that ends in .html, redirects to the page without .html
             source: '/:path*.html', 
             destination: '/:path*', // Redirects to the same path without .html
