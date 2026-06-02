2271c49d4b40b1dfc668347fdb1fad611b1f776d
Eline De Buyser <elinedebuyser@Smexmachine-2.local>
2026-06-02 16:16:46 +0800

    web, bug: desktop hero video never got loaded

--- apps/web/components/HeroVideo.tsx
+++ apps/web/components/HeroVideo.tsx
@@ -1,22 +1,9 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from "./HeroVideo.module.css";
 
 export default function HeroVideo() { 
-    const mobileVideo = { src: "/images/hero-video-mobile.mp4", height: 406, width: 720 };
-    const desktopVideo = { src: "/images/hero-video.mp4", height: 1080, width: 1920 };
-
-    const [video, setVideo] = useState(mobileVideo);
-
-    useEffect(() => {
-        const isMobile = window.innerWidth < 768;
-        setVideo(isMobile ? mobileVideo : desktopVideo);
-    }, []);
-
-    // todo: fallback image must have same aspect ratio as video!
-
     return (
         <div className={styles.heroContainer}>
             <Image
@@ -27,16 +14,19 @@ export default function HeroVideo() {
                 fetchPriority="high"
                 className="object-cover"
             />
-            <video autoPlay loop muted playsInline
-                className={styles.videoElement}
-                // poster="/images/hero-video-fallback.avif"
-                width={video.width} height={video.height}>
+            <video autoPlay loop muted playsInline className={styles.videoElement}>
+                {/* Desktop Source: Loaded only if the screen is 768px or wider */}
                 <source 
-                    src={video.src} 
-                    type="video/mp4"
+                    src="/images/hero-video.mp4" 
+                    type="video/mp4" 
+                    media="(min-width: 768px)" 
+                />
+                {/* Mobile Source: Fallback for anything smaller */}
+                <source 
+                    src="/images/hero-video-mobile.mp4" 
+                    type="video/mp4" 
                 />  
             </video>
-
             <div className={styles.overlay}>
                 <Image 
                     src="/images/logo-white-transparent-background.avif" 
