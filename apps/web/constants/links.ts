e48ba2012e4ebe6cc958795bcc74278946a24d23
Eline De Buyser <elinedebuyser@Smexmachine.lan>
2026-06-17 08:25:21 +0800

    web: updated activities pdf

--- apps/web/constants/links.ts
+++ apps/web/constants/links.ts
@@ -1,5 +1,5 @@
 export const EXT_LINKS = {
     FOOD_MENU_HH: 'https://www.canva.com/design/DAGhYRNTFgc/BkFZ5gh3rJnTuon_lI1OCg/view?utm_content=DAGhYRNTFgc&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hffa6aee53f',
     FOOD_MENU_JN: 'https://www.canva.com/design/DAGZCJNzgeA/L9Tz7Y8pdyin1PZQ9KeN7Q/view?utm_content=DAGZCJNzgeA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h99deea3dde',
-    ACTIVITIES_MENU: '/documents/activities-20260609.pdf',
+    ACTIVITIES_MENU: '/documents/activities-20260617.pdf',
 };
