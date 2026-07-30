d643767adcc3ff67cbbf90a364b855d7d66fb3da
Eline De Buyser <elinedebuyser@MacBookPro.lan>
2026-07-30 17:09:24 +0200

    web: add map to findUs section. make column div on small screens

--- apps/web/components/FindUs.tsx
+++ apps/web/components/FindUs.tsx
@@ -5,15 +5,19 @@ export default function FindUs({}) {
     return (
         <section className={styles.findUsSection}>
             <Image 
-                alt="Bali Tresure Map"
-                src={"/images/bali-map.png"}
-                width={500}
-                height={700}
+                src="/images/find-us-map.png"
+                alt="find us here"
+                width={600}
+                height={533}
+                style={{ 
+                    objectFit: 'cover',
+                    objectPosition: '40% 20%'
+                }}
             />
             <div className={styles.sectionBody}>
                 
             <h2 className={styles.findUsTitle}>
-                Find us in the heartlands of Bali
+                Find us in the <br/>heartlands of Bali
             </h2>
                 <div className={styles.bodyText}>
                     5 min - Tirta Empul Water Temple
@@ -28,9 +32,18 @@ export default function FindUs({}) {
                     <br/>
                     80552 Bali, Indonesia
                     <br/><br/>
-                    <button className={styles.mapsButton}>
-                        OPEN IN GOOGLE MAPS
-                    </button>
+                    
+                    <a 
+                        href="https://maps.app.goo.gl/CQWqgPz2JoxfKxji8" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <button 
+                            className={styles.mapsButton}
+                        >
+                            OPEN IN GOOGLE MAPS
+                        </button>
+                    </a>    
                 </div>
             </div>
         </section>
